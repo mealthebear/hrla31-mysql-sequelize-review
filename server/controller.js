e22de00750de47acb1ed5909c1174b20b9c69936
Dennis <chatkhan@Chatkhans-MacBook-Air.local>
2019-08-08 20:44:16 -0700

    Added more controller functions and seeder

--- server/controller.js
+++ server/controller.js
@@ -16,8 +16,20 @@ const controller = {
         const { name } = req.body;
         const { id } = req.params;
         Todo.update({ name }, { where: { id } })
-          .then(() => res.status(202).send(`Updated id ${id} with ${name}`))
+          .then((data) => {
+              if (data[0] === 1) {
+                res.status(202).send(`Updated id ${id} with ${name}`)
+              } else {
+                  res.status(202).send(`Could not update at id ${id}`)
+              }
+          })
           .catch(err => res.status(402).send(err));
+    },
+    delete: (req, res) => {
+        const { id } = req.params;
+        Todo.destroy({ where: { id } })
+        .then(() => res.status(203).send(`Deleted id ${id}`))
+        .catch(err => res.status(403).send(err))
     }
 }
 
